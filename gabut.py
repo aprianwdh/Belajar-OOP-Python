7702e4a403322c8d830efa5ad3720979a1fcab7f
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-12-31 11:21:54 +0700

    Create gabut.py

---
+++ gabut.py
@@ -0,0 +1,27 @@
+# #program untuk mengubah teks menjadi format tanggal
+# #import packages datetime
+# from datetime import datetime
+
+
+# hari = str(input("Masukkan tanggal: "))
+# bulan = str(input("Masukkan bulan (angka): "))
+# tahun = str(input("Masukkan tahun (lengkap): "))
+# jam = str(input("Masukkan jam: "))
+# menit = str(input("Masukkan menit: "))
+
+
+# tanggal = hari + ' ' + bulan + ' ' + tahun + ' ' + jam + ':' + menit
+
+
+# datetime_object = datetime.strptime(tanggal, "%d %m %Y %H:%M")
+
+
+# print(type(datetime_object))
+# print(datetime_object)
+
+
+list_nama = {"aku","kamu","dia"}
+
+for a,i in enumerate(list_nama):
+    print(f"Nilai dari index ke {a} = {i}")
+
